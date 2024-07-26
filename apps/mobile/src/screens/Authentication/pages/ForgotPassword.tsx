74733afba774d36b6b3bf14b0e272b9891ba1e08
Ameer Hamza <Ameerhamza.302@gmail.com>
2024-07-27 04:41:51 +0500

    forgot password UI done

--- apps/mobile/src/screens/Authentication/pages/ForgotPassword.tsx
+++ apps/mobile/src/screens/Authentication/pages/ForgotPassword.tsx
@@ -1,94 +1,110 @@
 import React from 'react';
-import { Button, Text, TextInput } from 'react-native-paper';
-import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { View, StyleSheet, useWindowDimensions } from 'react-native';
 
 // Utils
 import { theme } from '../../../theme';
 import { Icons } from '../../../assets';
 import { BackgroundLayout } from '../../../layouts';
-import {
-  Spacer,
-  StyledText,
-  BackButton,
-  CenterContainer,
-} from '../../../components';
-import { RouteNavigators, Routes } from '../../../navigation';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-// Fetches the translation for this particular page
+// // Fetches the translation for this particular page
 const getTransKey = (key) => `authentication.forgotPassword.${key}`;
 
+// Components
+import { Spacer, StyledText, BackButton } from '../../../components';
+
+const inputTheme = {
+  colors: {
+    text: theme.colors.white,
+    primary: theme.colors.white,
+    placeholder: theme.colors.white,
+    background: 'transparent',
+    underlineColor: 'transparent', // Hide the underline when focused
+  },
+};
+
 export const ForgotPassword = ({ navigation }) => {
+  const { width: windowWidth } = useWindowDimensions();
+
   const goBack = () => {
     navigation.goBack();
   };
 
-  const onResetPassword = () => {
-    navigation.navigate(Routes.Login);
+  const onResetPress = () => {
+    // Handle reset password action
   };
 
   return (
     <BackgroundLayout>
-      <BackButton onPress={goBack} />
-      <View style={styles.mainContainer}>
-        <CenterContainer>
-          <Spacer.md />
+      <KeyboardAwareScrollView
+        scrollEnabled={false}
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}
+      >
+        <BackButton onPress={goBack} />
+        <View style={styles.mainContainer}>
           <Icons.LiviinLogo />
           <StyledText.H4
             color={theme.colors.background}
             translationKey={getTransKey('heading')}
           />
-          <Spacer.xl />
-          <Spacer.md />
-          <View style={styles.inputContainer}>
-            <Spacer.sm />
-            <Button
-              mode="contained"
-              style={styles.button}
-              onPress={onResetPassword}
-              labelStyle={styles.label}
-              contentStyle={styles.buttonContent}
-            >
-              {'Send Email'}
-            </Button>
-          </View>
-        </CenterContainer>
-      </View>
+        </View>
+        <Spacer.xl />
+        <View style={styles.inputContainer}>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            placeholder="Type your email"
+            left={<TextInput.Icon color={theme.colors.white} name="email" />}
+            style={styles.input}
+            outlineColor={theme.colors.white}
+            theme={inputTheme}
+          />
+        </View>
+        <Spacer.md />
+        <Button
+          mode="contained"
+          style={[styles.resetButton, { width: windowWidth - 32, height: 40 }]}
+          onPress={onResetPress}
+          labelStyle={styles.label}
+          contentStyle={styles.buttonContent}
+        >
+          {'Reset Password'}
+        </Button>
+      </KeyboardAwareScrollView>
     </BackgroundLayout>
   );
 };
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    marginTop: 50,
-  },
-  textStyle: {
-    color: 'white',
-  },
-  container: {
-    flexDirection: 'row',
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: 16,
-    color: 'white',
+  mainContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '30%',
   },
-  button: {
+  inputContainer: {
+    width: '100%',
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
+    // backgroundColor: 'rgba(255, 255, 255, 0.1)', // Semi-transparent white background
+    borderRadius: 10,
+  },
+  resetButton: {
     borderRadius: 22,
-    backgroundColor: 'white',
+    backgroundColor: theme.colors.white,
   },
   buttonContent: {
     height: 40,
   },
   label: {
     color: '#4E5C4F',
-  },
-  inputContainer: {
-    flex: 1,
-    padding: 16,
-    justifyContent: 'center',
-  },
-  input: {
-    marginBottom: 16,
   },
 });
