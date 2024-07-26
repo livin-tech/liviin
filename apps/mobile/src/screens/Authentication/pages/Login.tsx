e3a3c0015c70240237447213f797ddd58f049501
Ameer Hamza <Ameerhamza.302@gmail.com>
2024-07-27 04:37:20 +0500

    Login screen UI implemented

--- apps/mobile/src/screens/Authentication/pages/Login.tsx
+++ apps/mobile/src/screens/Authentication/pages/Login.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Text, TextInput } from 'react-native-paper';
-import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+} from 'react-native';
 
 // Utils
 import { theme } from '../../../theme';
 import { Icons } from '../../../assets';
 import { BackgroundLayout } from '../../../layouts';
-import {
-  Spacer,
-  StyledText,
-  BackButton,
-  CenterContainer,
-} from '../../../components';
 import { RouteNavigators, Routes } from '../../../navigation';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+// Components
+import { Spacer, StyledText, BackButton } from '../../../components';
 
 // Fetches the translation for this particular page
 const getTransKey = (key) => `authentication.login.${key}`;
 
+const inputTheme = {
+  colors: {
+    text: theme.colors.white,
+    primary: theme.colors.white,
+    placeholder: theme.colors.white,
+    background: 'transparent',
+  },
+};
+
 export const Login = ({ navigation }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const { width: windowWidth } = useWindowDimensions();
+
   const goBack = () => {
     navigation.goBack();
   };
@@ -36,117 +51,124 @@ export const Login = ({ navigation }) => {
   };
 
   const onPressFaq = () => {};
+
   const onPressHelp = () => {};
 
   return (
     <BackgroundLayout>
-      <BackButton onPress={goBack} />
-      <View style={styles.mainContainer}>
-        <CenterContainer>
-          <Spacer.md />
+      <KeyboardAwareScrollView
+        scrollEnabled={false}
+        contentContainerStyle={styles.scrollView}
+        showsVerticalScrollIndicator={false}
+      >
+        <BackButton onPress={goBack} />
+        <View style={styles.mainContainer}>
           <Icons.LiviinLogo />
           <StyledText.H4
             color={theme.colors.background}
             translationKey={getTransKey('heading')}
           />
-          <Spacer.xl />
-          <TouchableOpacity onPress={onRegisterPress}>
-            <Text style={styles.loginText}>
-              Don't have an account? Register now
-            </Text>
+        </View>
+        <Spacer.sm />
+        <View style={styles.inputContainer}>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            placeholder="Type your email"
+            left={<TextInput.Icon color={theme.colors.white} name="email" />}
+            theme={inputTheme}
+            style={styles.input}
+            outlineColor={theme.colors.white}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            placeholder="Type your password"
+            secureTextEntry={!passwordVisible}
+            left={<TextInput.Icon color={theme.colors.white} name="lock" />}
+            right={
+              <TextInput.Icon
+                color={theme.colors.white}
+                name={passwordVisible ? 'eye-off' : 'eye'}
+                onPress={() => setPasswordVisible(!passwordVisible)}
+              />
+            }
+            theme={inputTheme}
+            style={styles.input}
+            outlineColor={theme.colors.white}
+          />
+        </View>
+        <View style={styles.forgotPasswordContainer}>
+          <TouchableOpacity onPress={onForgotPassword}>
+            <Text style={styles.forgotPasswordText}>Forgot password?</Text>
           </TouchableOpacity>
-          <Spacer.md />
-          <View style={styles.inputContainer}>
-            {/* <TextInput
-              mode="outlined"
-              label="Email"
-              placeholder="Enter your email"
-              left={<TextInput.Icon name="email" />}
-              style={styles.input}
-            />
-            <TextInput
-              mode="outlined"
-              label="Password"
-              placeholder="Enter your password"
-              secureTextEntry
-              left={<TextInput.Icon name="lock" />}
-              style={styles.input}
-            /> */}
-            <TouchableOpacity
-              onPress={onForgotPassword}
-              style={styles.forgotPasswordContainer}
-            >
-              <Text style={styles.forgotPasswordText}>Forgot password?</Text>
-            </TouchableOpacity>
-            <Spacer.sm />
-            <Button
-              mode="contained"
-              style={styles.button}
-              onPress={onLoginPress}
-              labelStyle={styles.label}
-              contentStyle={styles.buttonContent}
-            >
-              {'Login'}
-            </Button>
-          </View>
-          <View style={styles.buttonsContainer}>
-            <Button
-              mode="contained"
-              onPress={onPressFaq}
-              style={styles.button}
-              labelStyle={styles.label}
-              contentStyle={styles.buttonContent}
-            >
-              {'FAQ'}
-            </Button>
-            <Button
-              mode="contained"
-              onPress={onPressHelp}
-              style={styles.button}
-              labelStyle={styles.label}
-              contentStyle={styles.buttonContent}
-            >
-              {'Help'}
-            </Button>
-          </View>
-        </CenterContainer>
+        </View>
+        <Spacer.md />
+        <Button
+          mode="contained"
+          style={[styles.loginButton, { width: windowWidth - 32, height: 40 }]}
+          onPress={onLoginPress}
+          labelStyle={styles.label}
+          contentStyle={styles.buttonContent}
+        >
+          {'Login'}
+        </Button>
+      </KeyboardAwareScrollView>
+      <View style={styles.footerContainer}>
+        <TouchableOpacity onPress={onRegisterPress}>
+          <Text style={styles.loginText}>
+            Don't have an account?{' '}
+            <Text style={styles.underline}>Register</Text>
+          </Text>
+        </TouchableOpacity>
+        <Spacer.md />
+        <View style={styles.buttonsContainer}>
+          <Button
+            mode="contained"
+            onPress={onPressFaq}
+            style={[styles.footerButton, { marginRight: 15 }]}
+            labelStyle={styles.label}
+            contentStyle={styles.buttonContent}
+          >
+            {'FAQ'}
+          </Button>
+          <Button
+            mode="contained"
+            onPress={onPressHelp}
+            style={styles.footerButton}
+            labelStyle={styles.label}
+            contentStyle={styles.buttonContent}
+          >
+            {'Help'}
+          </Button>
+        </View>
       </View>
     </BackgroundLayout>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   mainContainer: {
-    marginTop: 50,
-  },
-  textStyle: {
-    color: 'white',
-  },
-  container: {
-    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-  },
-  buttonsContainer: {
-    width: '55%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-    color: 'white',
+    height: '30%',
   },
   underline: {
     textDecorationLine: 'underline',
-    color: 'white',
+    color: theme.colors.white,
   },
   loginText: {
     fontSize: 16,
-    color: 'white',
+    color: theme.colors.white,
   },
-  button: {
+  loginButton: {
     borderRadius: 22,
-    backgroundColor: 'white',
+    backgroundColor: theme.colors.white,
   },
   buttonContent: {
     height: 40,
@@ -154,19 +176,35 @@ const styles = StyleSheet.create({
   label: {
     color: '#4E5C4F',
   },
-  inputContainer: {
-    flex: 1,
-    padding: 16,
+  footerContainer: {
+    marginBottom: 25,
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    marginBottom: 16,
+  buttonsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerButton: {
+    borderRadius: 22,
+    backgroundColor: theme.colors.white,
   },
   forgotPasswordContainer: {
+    width: '100%',
     alignItems: 'flex-end',
+    paddingHorizontal: 16,
   },
   forgotPasswordText: {
     color: 'white',
+    fontWeight: 'bold',
     textDecorationLine: 'underline',
+  },
+  inputContainer: {
+    width: '100%',
+    padding: 16,
+  },
+  input: {
+    marginBottom: 16,
   },
 });
