d5e348f0b9f89a70c0fbe777928d94a0852cecee
ifahadashraf <fahadashraf9612@gmail.com>
2024-08-08 23:49:34 +0200

    remote mongo uri added

--- apps/backend/src/database/data-source.ts
+++ apps/backend/src/database/data-source.ts
@@ -5,8 +5,10 @@ dotenv.config();
 
 // Connect to MongoDB using Mongoose
 export const connectToDatabase = async () => {
+  const remoteDB = 'mongodb+srv://fahadashraf9612:JN2FfuxlLjogDnfG@cluster0.v5nfpvl.mongodb.net/liviin_db?retryWrites=true&w=majority&appName=Cluster0';
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/liviin_db');
+    await mongoose.connect(remoteDB || 'mongodb://localhost:27017/liviin_db');
     console.log('Successfully connected to MongoDB!');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
