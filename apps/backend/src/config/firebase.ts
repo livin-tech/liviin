80925702d4141f02b635c03715b51d9ec43bd4e8
Ameer Hamza <Ameerhamza.302@gmail.com>
2024-10-02 21:43:09 +0400

    Minor updates

--- apps/backend/src/config/firebase.ts
+++ apps/backend/src/config/firebase.ts
@@ -1,9 +1,9 @@
 import admin from 'firebase-admin';
 
-import serviceAccount from '../firebase-service.json';
+// import serviceAccount from '../firebase-service.json';
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+// admin.initializeApp({
+//   // credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
 
 export default admin
