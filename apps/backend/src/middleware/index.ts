622b2af812ec5ed516f131a4576712c89a810bdb
ifahadashraf <fahadashraf9612@gmail.com>
2024-10-06 00:40:11 +0200

    removed token auth

--- apps/backend/src/middleware/index.ts
+++ apps/backend/src/middleware/index.ts
@@ -3,21 +3,21 @@ import { NextFunction, Request, Response } from 'express';
 // import admin from '../config/firebase';
 
 export const verifyToken = async (req: Request & { user?: any }, res: Response, next: NextFunction): Promise<void> => {
-  const authHeader = req.headers.authorization;
+  // const authHeader = req.headers.authorization;
+  //
+  // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  //   res.status(403).json({ error: 'No token provided or incorrect format' });
+  //   return;
+  // }
+  //
+  // const idToken = authHeader.split(' ')[1];
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    res.status(403).json({ error: 'No token provided or incorrect format' });
-    return;
-  }
-
-  const idToken = authHeader.split(' ')[1]; 
-
-  try {
+  // try {
     // const decodedToken = await admin.auth().verifyIdToken(idToken);
     // req.user = decodedToken; // Attach the decoded token to the request
     next();
-  } catch (error) {
-    console.error('Error verifying token:', error);
-    res.status(403).json({ error: 'Unauthorized' });
-  }
+  // } catch (error) {
+  //   console.error('Error verifying token:', error);
+  //   res.status(403).json({ error: 'Unauthorized' });
+  // }
 };
