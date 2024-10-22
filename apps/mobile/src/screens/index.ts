d903421df8bb721fb98fcd99d9f9e729c3cd7704
Ameer Hamza <Ameerhamza.302@gmail.com>
2024-10-23 03:35:02 +0400

    terms renamed

--- apps/mobile/src/screens/index.ts
+++ apps/mobile/src/screens/index.ts
@@ -1,10 +1,9 @@
 export * from './Tasks';
 export * from './Authentication';
-
 export { Home } from './Home';
 export { FAQs } from './FAQs';
 export { Help } from './Help';
-export { Terms } from './terms';
+export { Terms } from './Terms';
 export { Profile } from './Profile';
 export { Landing } from './Landing';
 export { CalendarScreen } from './Calendar';
